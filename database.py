818b9fb0116ff2fe0a29357c8758c712784ec7c3
Rayhan Rizqial <rrizqialhariz@gmail.com>
2024-10-07 10:21:25 +0700

    Udh di env

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -6,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 # Configure the MySQL database connection (using PyMySQL as driver)
-DATABASE_URL = "mysql+pymysql://root:mypassword@db:3307/bigproject"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Create the engine
 engine = create_engine(DATABASE_URL, echo=True)  # echo=True for SQL logging
